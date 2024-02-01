ebd12930d2c99f22a0968acd525f201ac34abbe4
taner <tanerozer16@gmail.com>
2024-02-01 13:50:40 +0300

    auth route added

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+// routes/auth.js
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController.js");
+
+router.post("/register", authController.registerUser);
+router.post("/login", authController.loginUser);
+
+module.exports = router;
